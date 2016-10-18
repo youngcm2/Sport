329bda5991cd4cb7f6c5da97b8dfbb8644ff07bb
Christopher Young <youngcm2>
2016-10-18 13:58:10 -0500

    Updated keys

--- Sport.Mobile.Shared/Keys.cs
+++ Sport.Mobile.Shared/Keys.cs
@@ -2,13 +2,13 @@
 {
 	public class Keys
 	{
-		public static readonly string SourceCodeUrl = "https://github.com/xamarin/sport";
+		public static readonly string SourceCodeUrl = "https://github.com/youngcm2/sport";
 		public static readonly string GooglePushNotificationSenderId = "REPLACE_WITH_YOUR_GOOGLE_SENDER_ID_FOR_PUSH_NOTIFICATIONS";
 		public static readonly string HockeyAppId_Android = "REPLACE_WITH_YOUR_HOCKEYAPP_KEY";
-		public static readonly string HockeyAppId_iOS = "REPLACE_WITH_YOUR_HOCKEYAPP_KEY";
+		public static readonly string HockeyAppId_iOS = "a1ebf4157e484c569131fc0cb72052ca";
 
-        public static readonly string AzureDomainLocal = "http://10.211.55.3/Sport.Service/";
-        public static readonly string AzureDomainRemote = "https://xamarin-sportv2.azurewebsites.net/";
-        public static string AzureDomain = AzureDomainRemote;
-    }
+		public static readonly string AzureDomainLocal = "http://10.211.55.3/Sport.Service/";
+		public static readonly string AzureDomainRemote = "https://xamarin-sportv2.azurewebsites.net/";
+		public static string AzureDomain = AzureDomainRemote;
+	}
 }
